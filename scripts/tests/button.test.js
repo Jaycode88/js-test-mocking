ac5160bb78ede1fc634342929e9c88cc6f693155
Jaycode88 <>
2023-06-07 11:39:28 +0000

    Jest mock dom test complete

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,14 +4,20 @@
 
 const buttonClick = require("../button");
 
-beforeAll(() => {
+//the beforeEach function is a hook that allows you to define a setup or
+//initialization step that will run before each test case in a test suite
+//or test block. It is commonly used to set up a consistent environment or
+//state for each test, ensuring that tests are independent and predictable.
+//The beforeEach function takes a callback function as an argument.This callback
+//function contains the code that will be executed before each test case.
+
+beforeEach(() => {
     document.body.innerHTML = "<p id='par'></p>";
 });
 
 describe("DOM tests", () => {
     test("Expects content to change", () => {
         buttonClick();
-        expect(document.getElementById("par")
-            .innerHTML).toEqual("You Clicked");
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
     });
 });
